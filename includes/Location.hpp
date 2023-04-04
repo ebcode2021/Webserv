a07399fd9bac68a4144b4da6fda485a1ea92ef0c
ebcode2021 <ebcode2021@gmail.com>
2023-04-04 20:08:42 +0900

    [add] testcode

---
+++ includes/Location.hpp
@@ -0,0 +1,20 @@
+#ifndef LOCATION_HPP
+# define LOCATION_HPP
+
+# include <iostream>
+# include <string>
+# include <vector>
+
+class Location {
+
+	private :
+		std::string _path;
+		std::string _root;
+		std::vector<std::string> _index;
+
+	public :
+		Location();
+		~Location();
+};
+
+#endif
---
+++ includes/Server.hpp
@@ -0,0 +1,17 @@
+#ifndef SERVER_HPP
+# define SERVER_HPP
+
+# include <iostream>
+# include <string>
+# include <vector>
+
+class Server {
+
+	private :
+		
+	public :
+	
+};
+
+#endif
+
