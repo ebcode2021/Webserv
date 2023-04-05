74a39b8eb38a8b9d104eb5a9a8c11ae78a836d57
ebcode2021 <ebcode2021@gmail.com>
2023-04-05 15:51:09 +0900

    Revert "[add] testcode"

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,7 @@
-#include "Webserv.hpp"
-
-void	config(std::string fileName)
-{
-	// open 하면서 readline으로 한줄씩 읽겠지.
-
-	// 1. 한번에 에러 + 객체 생성
-	// 2. 함수로 뺄지, 클래스로 보관할지?
-	// error check
-	// block 3개 
-
-	// 라이브러리..?
-}
+#include <iostream>
+#include <fstream>
 
 int main(int argc, char *argv[])
 {
-	if (validateArgument(argc, argv))
-	{
-		// serverList 반환
-		config(argv[1]); // conf 파싱
-		// 통신 시작 함수
-	}
 	return 0;
 }
